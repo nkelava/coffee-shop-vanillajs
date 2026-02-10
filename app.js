7b7c9ede7477d99f45bbfb1e719e74d407522189
Nikola Kelava <nkelava.dev@gmail.com>
2026-02-10 09:20:19 +0100

    feat: call loadMenu on DOMContentLoaded

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import Store from "./services/Store.js";
+import { loadMenu } from "./services/Menu.js";
 
 window.app = {
   store: Store,
 };
 
 // Wait for the DOM to be fully loaded before accessing or modifying it.
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  loadMenu();
+});
