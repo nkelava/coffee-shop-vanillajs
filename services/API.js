71cf981cf871772f2437087f9d8085ee1052c1ef
Nikola Kelava <nkelava.dev@gmail.com>
2026-02-10 09:16:03 +0100

    fix: use API.url instead of this.url to fix undefined reference

--- services/API.js
+++ services/API.js
@@ -1,7 +1,7 @@
 const API = {
   url: "/data/menu.json",
   fetchMenu: async () => {
-    const result = await fetch(this.url);
+    const result = await fetch(API.url);
     return await result.json();
   },
 };
