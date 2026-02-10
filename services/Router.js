69c42d04116969e8d4d2c8405d2f8676adc37fa0
Nikola Kelava <nkelava.dev@gmail.com>
2026-02-10 22:41:37 +0100

    feat: support back/forward navigation and route-based rendering

--- services/Router.js
+++ services/Router.js
@@ -9,6 +9,11 @@ const Router = {
       });
     });
 
+    // Event Handler for URL changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
+
     Router.go(location.pathname);
   },
   go: (route, addToHistory = true) => {
@@ -21,7 +26,35 @@ const Router = {
         route,
       );
     }
+
+    let pageElement = null;
+
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Your Order";
+        break;
+      default:
+        if (route.startsWith("/product/")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Details";
+        }
+    }
   },
 };
+
+if (pageElement) {
+  const mainElement = document.querySelector("main");
+
+  mainElement?.children[0]?.remove();
+  mainElement.appendChild(pageElement);
+
+  window.scrollX = 0;
+  window.scrollY = 0;
+}
 
 export default Router;
