2301da2448f7e637b953ed16830a18b76bc5e6e5
Nikola Kelava <nkelava.dev@gmail.com>
2026-02-11 22:12:54 +0100

    feat: add Shadow DOM and scoped CSS loading

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,26 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super();
+
+    this.root = this.attachShadow({ mode: "open" });
+
+    const styleElement = document.createElement("style");
+    this.root.appendChild(styleElement);
+
+    async function loadCSS() {
+      const response = await fetch("/components/MenuPage.css");
+      const cssText = await response.text();
+      styleElement.textContent = cssText;
+    }
+
+    loadCSS();
+  }
+
+  // When the component is attached to the DOM
+  connectedCallback() {
+    const template = document.getElementById("menu-page-template");
+    const content = template.content.cloneNode(true);
+    this.root.appendChild(content);
   }
 }
 
